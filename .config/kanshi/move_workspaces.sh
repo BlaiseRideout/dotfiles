6e78b84dc37a270864e6e3ffefd7b85ee82b2c2f
Blaise Rideout <blaise@blaise.in>
2024-05-24 10:41:09 -0700

    Add printsrc+dnd to sway/mako, update kanshi

--- .config/kanshi/move_workspaces.sh
+++ .config/kanshi/move_workspaces.sh
@@ -2,16 +2,22 @@
 
 outputs=$(swaymsg -t get_outputs | jq '[.[] | select(.name!="eDP-1")] | sort_by(.current_mode) | .[].name')
 
-if [[ $(echo outputs | wc -l) -eq 1 ]]; then
-  echo "dual mode"
+NUM=$(echo $outputs | wc -w)
+
+if [[ $NUM -eq 1 ]]; then
+  echo "dual mode, primary $outputs"
   swaymsg workspace 9, move workspace to eDP-1
   swaymsg workspace 10, move workspace to eDP-1
-  swaymsg workspace 1, move workspace to $outputs
-elif [[ $(outputs | wc -l) -eq 2 ]]; then
-  echo "triple mode"
-  mon1="$(echo $outputs | cut -d' ' -f 1)"
-  mon2="$(echo $outputs | cut -d' ' -f 2)"
-  swaymsg workspace 9, move workspace to $mon1
+  for I in $(seq 8 -1 1); do
+    swaymsg workspace $I, move workspace to $outputs
+  done
+elif [[ $NUM -eq 2 ]]; then
+  secondary="$(echo $outputs | cut -d' ' -f 1)"
+  primary="$(echo $outputs | cut -d' ' -f 2)"
+  echo "triple mode, primary $primary secondary $secondary"
+  swaymsg workspace 9, move workspace to $secondary
   swaymsg workspace 10, move workspace to eDP-1
-  swaymsg workspace 1, move workspace to $mon2
+  for I in $(seq 8 -1 1); do
+    swaymsg workspace $I, move workspace to $primary
+  done
 fi
